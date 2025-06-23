bd27a5789ee88c0a9033b33549892c4853ef8dc3
Rick Haffey <rickhaffey@gmail.com>
2025-06-23 07:47:24 -0500

    refactor out initial data access layer

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,16 +1,11 @@
 from opentelemetry import metrics
 from .utils.observability import instrument
 from fastapi import FastAPI
-import psycopg
-from pydantic import BaseModel
-from psycopg.rows import class_row
-import os
-from loguru import logger
-import time
-import random
 
 from .routers import observability_demo
 from .utils.observability_middleware import MetricsMiddleware
+from .models.recipe import Recipe
+from .data_access.recipe_repository import RecipeRepository
 
 app = FastAPI()
 
@@ -24,42 +19,13 @@ work_counter = meter.create_counter(
     "request.counter", unit="1", description="Counts the number of requests made"
 )
 
-
-class Recipe(BaseModel):
-    id: int
-    name: str
-
-
-def get_connection():
-    host = os.environ["POSTGRES_SERVER"]
-    port = os.environ["POSTGRES_PORT"]
-    dbname = os.environ["POSTGRES_DB"]
-    user = os.environ["POSTGRES_USER"]
-    password = os.environ["POSTGRES_PASSWORD"]
-
-    return psycopg.connect(
-        f"host={host} port={port} dbname={dbname} user={user} password={password}"
-    )
+repository = RecipeRepository()
 
 
 @app.get("/recipes")
-def read_recipes():
-    time.sleep(random.randint(5, 10))
+def read_recipes() -> list[Recipe]:
     work_counter.add(1, {"work.type": "read_recipe"})
-    result = []
-
-    logger.debug("read_recipes: LOG MESSAGE DEMO - DEBUG")
-    logger.info("read_recipes: LOG MESSAGE DEMO - INFO")
-    logger.warning("read_recipes: LOG MESSAGE DEMO - WARNING")
-    logger.error("read_recipes: LOG MESSAGE DEMO - ERROR")
-    logger.critical("read_recipes: LOG MESSAGE DEMO - CRITICAL")
-
-    with get_connection() as conn, conn.cursor(row_factory=class_row(Recipe)) as cur:
-        cur.execute("SELECT * FROM recipes")
-        for record in cur:
-            result.append(record)
-
-    return result
+    return repository.get_all_recipes()
 
 
 instrument(app, service_name="lloyd-api")
---
+++ api/src/api/models/recipe.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+
+class Recipe(BaseModel):
+    id: int
+    name: str
