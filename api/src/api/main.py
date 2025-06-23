bf20d8bce7fe494e274a369ceb2cea7132a848b5
Rick Haffey <rickhaffey@gmail.com>
2025-06-23 07:54:41 -0500

    initial recipes router

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,31 +1,15 @@
-from opentelemetry import metrics
 from .utils.observability import instrument
 from fastapi import FastAPI
 
 from .routers import observability_demo
+from .routers import recipes
 from .utils.observability_middleware import MetricsMiddleware
-from .models.recipe import Recipe
-from .data_access.recipe_repository import RecipeRepository
 
 app = FastAPI()
 
 app.add_middleware(MetricsMiddleware, app_name="app-a")
 
 app.include_router(observability_demo.router)
-
-meter = metrics.get_meter("lloyd.otel.meter")
-
-work_counter = meter.create_counter(
-    "request.counter", unit="1", description="Counts the number of requests made"
-)
-
-repository = RecipeRepository()
-
-
-@app.get("/recipes")
-def read_recipes() -> list[Recipe]:
-    work_counter.add(1, {"work.type": "read_recipe"})
-    return repository.get_all_recipes()
-
+app.include_router(recipes.router)
 
 instrument(app, service_name="lloyd-api")
